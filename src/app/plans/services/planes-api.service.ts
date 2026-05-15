4be4558ac129c3a03250684635e09028e15e6613
Rum1z <134657480+DavidRm1911@users.noreply.github.com>
2026-05-14 20:55:48 -0500

    fix: replace hardcoded Azure URL with environment.serverBasePath in plans service

--- src/app/plans/services/planes-api.service.ts
+++ src/app/plans/services/planes-api.service.ts
@@ -1,31 +1,19 @@
 import {Injectable, inject} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanesApiService {
-  private specificCategory: string  = 'plan2';
-  //private baseUrl: string = 'https://my-json-server.typicode.com/Open-Source-SW56-Group-2-EcoMovil/Json-Server-EcoMovil';
-  private baseUrl = 'https://app-250421124000.azurewebsites.net/api/v1'
+  private specificCategory: string  = 'plans';
+  private baseUrl = `${environment.serverBasePath}`;
   private http: HttpClient = inject(HttpClient);
 
-  /**
-   * @method getPlans
-   * @description
-   * Fetches the plans.
-   * @returns An observable of the plans.
-   */
   getSources() {
     return this.http.get(`${this.baseUrl}/${this.specificCategory}`);
   }
 
-  /**
-   * @method initPlans
-   * @description
-   * Initializes the plans.
-   * @returns An observable of the plans.
-   */
   initUniversities() {
     return this.getSources();
   }
