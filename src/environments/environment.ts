c80620d6b6b32fa62a03c30d8f989edbf2323211
Rum1z <134657480+DavidRm1911@users.noreply.github.com>
2026-06-13 08:00:58 -0500

    chore: update CloudFront domain after infrastructure redeploy
    New domain: d303dj5b0hedv6.cloudfront.net

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverBasePath: 'https://dri7twtno221s.cloudfront.net/api/v1',
+  serverBasePath: 'https://d303dj5b0hedv6.cloudfront.net/api/v1',
   googleMapsKey: 'GOOGLE_MAPS_KEY_PLACEHOLDER'
 };
