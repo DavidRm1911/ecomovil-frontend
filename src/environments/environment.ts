f3d192725778e40e860b47a50334658d79bf723b
Rum1z <134657480+DavidRm1911@users.noreply.github.com>
2026-06-18 15:31:36 -0500

    chore: update CloudFront domain after destroy/apply test cycle

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverBasePath: 'https://d1yu8hfos2iugr.cloudfront.net/api/v1',
+  serverBasePath: 'https://d1gq6r8sv8rmyg.cloudfront.net/api/v1',
   googleMapsKey: 'GOOGLE_MAPS_KEY_PLACEHOLDER'
 };
