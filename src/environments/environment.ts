d1c85f95e646bfc9c0185f9788c8e9d34cdba685
Rum1z <134657480+DavidRm1911@users.noreply.github.com>
2026-06-19 11:10:11 -0500

    chore: update CloudFront domain after terraform apply

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverBasePath: 'https://d1gq6r8sv8rmyg.cloudfront.net/api/v1',
+  serverBasePath: 'https://d1qdb0mr26sxfs.cloudfront.net/api/v1',
   googleMapsKey: 'GOOGLE_MAPS_KEY_PLACEHOLDER'
 };
