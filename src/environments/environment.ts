39e8b8460cf6876d4afeb4e9843c41ad940fb0ad
Rum1z <134657480+DavidRm1911@users.noreply.github.com>
2026-07-07 16:30:57 -0500

    update CloudFront URL to dtbi26epuh1r2 (new terraform apply)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverBasePath: 'https://d1qdb0mr26sxfs.cloudfront.net/api/v1',
+  serverBasePath: 'https://dtbi26epuh1r2.cloudfront.net/api/v1',
   googleMapsKey: 'GOOGLE_MAPS_KEY_PLACEHOLDER'
 };
